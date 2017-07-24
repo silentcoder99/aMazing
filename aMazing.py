bdbf4933fe391208bd9e4d2ddeefcf707dd21a0d
Sam Nolan <samnolan555@gmail.com>
2017-07-24 14:23:13 +1000

    Randomized edge weights
    Places random edge weights in a dictionary of edges that can be
    referenced later

--- aMazing.py
+++ aMazing.py
@@ -1,1 +1,15 @@
 from min_heap import MinHeap
+from random import random
+edgeWeights = {}
+
+# size of grid
+gridSize = 8
+
+for i in range(0,gridSize):
+	for j in range(0,gridSize):
+		if i != gridSize - 1:
+			edgeWeights[frozenset({(i, j), (i + 1, j)})] = random()
+		if j != gridSize - 1:
+			edgeWeights[frozenset({(i, j), (i, j + 1)})] = random()
+
+print edgeWeights
