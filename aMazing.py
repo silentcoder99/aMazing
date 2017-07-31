83d9b7e04ed3f06d5b5aeb72de77148bef091795
silentcoder99 <silentcoder99@gmail.com>
2017-07-31 15:38:38 +1000

    Add frame output
    Maze generation animation is outputed as a series of PNG images to be converted to a movie file

--- aMazing.py
+++ aMazing.py
@@ -3,7 +3,9 @@ from queue import PriorityQueue
 import sys
 from random import random
 import render
+from render import GifBuilder
 from PIL import Image
+import math
 
 edgeWeights = {}
 
@@ -19,11 +21,20 @@ elif(len(sys.argv) == 3):
         gridWidth = sys.argv[1]
         gridHeight = sys.argv[2]
 else:
-        gridWidth = int((width - 1) / 2)
-        gridHeight = int((height - 1) / 2)
+        # gridWidth = int((width - 1) / 2)
+        # gridHeight = int((height - 1) / 2)
+
+        #big mazes take a long time to generate frames
+        gridWidth = 50
+        gridHeight = 50
 
 #image scale (1 - 1:1 pixel ratio)
 scale = 8
+
+#create instance of GifBuilder
+gif = GifBuilder(gridWidth * 2 + 1, gridHeight * 2 + 1, scale)
+#add entrance to gif
+gif.add_frame(0, 1)
 
 def getWeight(position):
         return grayscale.getpixel(position)
@@ -31,104 +42,125 @@ def getWeight(position):
 
 # Generate random edge weights
 for i in range(0,gridWidth):
-	for j in range(0,gridHeight):
-		if i != gridWidth - 1:
-			edgeWeights[frozenset({(i, j), (i + 1, j)})] = getWeight((2 * i + 1, 2 * j))
-		if j != gridHeight - 1:
-			edgeWeights[frozenset({(i, j), (i, j + 1)})] = getWeight((2 * i, 2 * j + 1))
+    for j in range(0,gridHeight):
+        if i != gridWidth - 1:
+            edgeWeights[frozenset({(i, j), (i + 1, j)})] = getWeight((2 * i + 1, 2 * j))
+        if j != gridHeight - 1:
+            edgeWeights[frozenset({(i, j), (i, j + 1)})] = getWeight((2 * i, 2 * j + 1))
 
 treeNodes = set()
 treeEdges = set()
 openEdges = PriorityQueue()
 
 def getNeighbors(point):
-	nodes = set()
-	x = point[0]
-	y = point[1]
-	if x > 0:
-		nodes.add((x-1, y))
-	if y > 0:
-		nodes.add((x, y-1))
-	if x < gridWidth - 1:
-		nodes.add((x + 1, y))
-	if y < gridHeight - 1:
-		nodes.add((x, y+1))
-	return nodes
+    nodes = set()
+    x = point[0]
+    y = point[1]
+    if x > 0:
+        nodes.add((x-1, y))
+    if y > 0:
+        nodes.add((x, y-1))
+    if x < gridWidth - 1:
+        nodes.add((x + 1, y))
+    if y < gridHeight - 1:
+        nodes.add((x, y+1))
+    return nodes
 
 startNode = (0,0)
 # Add our first node to the tree
 treeNodes.add(startNode)
+gif.add_frame(1, 1)
 
 # Discover neighbors from that node
 rootNeighbors = getNeighbors(startNode)
 
 # Add those neighbors
 for rootNeighbor in rootNeighbors:
-	rootEdge = frozenset({startNode, rootNeighbor})
-	openEdges.put((edgeWeights[rootEdge],rootEdge))
+    rootEdge = frozenset({startNode, rootNeighbor})
+    openEdges.put((edgeWeights[rootEdge],rootEdge))
 
 # Do it for the rest
 while not openEdges.empty():
-	edge = openEdges.get()
+    edge = openEdges.get()
 
-	# Check the two endpoints for new nodes
-	for endpointNode in edge[1]:
-		if not endpointNode in treeNodes:
-			# add edge to treeEdges
-			treeEdges.add(edge[1])
-			# Add the new node to the tree
-			treeNodes.add(endpointNode)
+    # Check the two endpoints for new nodes
+    for endpointNode in edge[1]:
+        if not endpointNode in treeNodes:
+            #find node already in treeNodes
+            originNode = (0, 0)
+            for node in edge[1] - frozenset({endpointNode}):
+                originNode = node
 
-			# explore the new node for edges
-			neighbors = getNeighbors(endpointNode)
+            # add edge to treeEdges
+            treeEdges.add(edge[1])
+            # Add the new node to the tree
+            treeNodes.add(endpointNode)
 
-			for neighbor in neighbors:
-				if not neighbor in treeNodes:
-					newEdge = frozenset({endpointNode, neighbor})
-					openEdges.put((edgeWeights[newEdge], newEdge))
+            #add gif frames
+            #average origin and new node co-ords to find edge position
+            gif.add_frame(originNode[0] + endpointNode[0] + 1, originNode[1] + endpointNode[1] + 1)
+            #add new node
+            gif.add_frame(endpointNode[0] * 2 + 1, endpointNode[1] * 2 + 1)
+
+            #print progress percent
+            print(len(treeNodes)/(gridWidth * gridHeight))
+
+            # explore the new node for edges
+            neighbors = getNeighbors(endpointNode)
+
+            for neighbor in neighbors:
+                if not neighbor in treeNodes:
+                    newEdge = frozenset({endpointNode, neighbor})
+                    openEdges.put((edgeWeights[newEdge], newEdge))
+
+#add exit to movie
+gif.add_frame(gridWidth * 2, gridHeight * 2 - 1)
+
+#---Generate Pixel Data---
 
 #holds data to be written to bitmap
 pixelData = []
 
 #add upper edge
 for i in range(0, gridWidth * 2 + 1):
-		pixelData.append(0)
+        pixelData.append(0)
 
 for j in range(0,gridHeight):
 
-	#Left border
-	if j == 0:
-		pixelData.append(1)
-	else:
-		pixelData.append(0)
+    #Left border
+    if j == 0:
+        pixelData.append(1)
+    else:
+        pixelData.append(0)
 
-	#right edges
-	for i in range(0,gridWidth):
+    #right edges
+    for i in range(0,gridWidth):
 
-		#add pixel for each node
-		pixelData.append(1)
+        #add pixel for each node
+        pixelData.append(1)
 
-		#add pixel for each right edge in treeEdges
-		if j == gridHeight - 1 and i == gridWidth - 1:
-			pixelData.append(1)
-		else:
-			if frozenset({(i, j),(i+1,j)}) in treeEdges:
-				pixelData.append(1)
-			else:
-				pixelData.append(0)
+        #add pixel for each right edge in treeEdges
+        if j == gridHeight - 1 and i == gridWidth - 1:
+            pixelData.append(1)
+        else:
+            if frozenset({(i, j),(i+1,j)}) in treeEdges:
+                pixelData.append(1)
+            else:
+                pixelData.append(0)
 
-	# Left border
-	pixelData.append(0)
+    # Left border
+    pixelData.append(0)
 
-	#add pixel for each down edge in treeEdges
-	for i in range(0, gridWidth):
+    #add pixel for each down edge in treeEdges
+    for i in range(0, gridWidth):
 
-		if frozenset({(i, j),(i,j+1)}) in treeEdges:
-			pixelData.append(1)
-		else:
-			pixelData.append(0)
+        if frozenset({(i, j),(i,j+1)}) in treeEdges:
+            pixelData.append(1)
+        else:
+            pixelData.append(0)
 
-		pixelData.append(0)
+        pixelData.append(0)
 
 print(len(pixelData))
 render.show_maze(pixelData, gridWidth * 2 + 1, gridHeight * 2 + 1, scale)
+#gif.build_gif()
--- render.py
+++ render.py
@@ -1,4 +1,8 @@
 from PIL import Image
+import os
+
+#absolute path of script
+path = os.path.dirname(os.path.abspath(__file__))
 
 def show_maze(pixelData, sizeX, sizeY, scale):
     #create black image
@@ -14,5 +18,43 @@ def show_maze(pixelData, sizeX, sizeY, scale):
     im.show()
     im.save("maze.png")
 
-def show_gif():
-    pass
+class GifBuilder:
+    def __init__(self, sizeX, sizeY, scale):
+        self.frameCount = 0
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+        self.scale = scale
+        self.lastFrame = Image.new("1", (self.sizeX, self.sizeY))
+        im = self.lastFrame.resize((self.sizeX * scale, self.sizeY * scale))
+
+        #create frames directory if one doesn't exist
+        if not os.path.exists(path + "\\frames"):
+            os.makedirs(path + "\\frames")
+
+        #save initial frame
+        im.save(path + "\\frames\\frame" + str(self.frameCount) + ".png")
+        self.frameCount += 1
+
+    def add_frame(self, x, y):
+        #saves a copy of last frame with added pixel at (x, y)
+        newFrame = self.lastFrame.copy()
+        newFrame.putpixel((x, y), 1)
+        self.lastFrame = newFrame
+        newFrame = newFrame.resize((self.sizeX * self.scale, self.sizeY * self.scale))
+        newFrame.save(path + "\\frames\\frame" + str(self.frameCount) + ".png")
+
+        self.frameCount += 1
+
+    def build_gif(self):
+        #resize all frames based on scale
+        try:
+            for i in range(0, len(self.frames)):
+                self.frames[i] = self.frames[i].resize((self.sizeX * self.scale, self.sizeY * self.scale))
+        except ValueError:
+            print("Bad Image Mode:", self.frames[i].mode)
+            self.frames[i].show()
+            raise
+
+        #create gif from frames
+        with Image.open("maze.gif") as im:
+            im.save("maze.gif", save_all = True, append_images = self.frames, duration = delay)
